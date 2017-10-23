917813afff18d30bd2067ed2a99cc0aa81083527
lizhiyuan <148334733@qq.com>
2017-10-23 21:48:07 +0800

    放入购物车的后端实现，前端未完成

--- src/views/GoodList.vue
+++ src/views/GoodList.vue
@@ -44,7 +44,7 @@
                     <div class="name">{{item.productName}}</div>
                     <div class="price">{{item.salePrice | currency('$')}}</div>
                     <div class="btn-area">
-                      <a href="javascript:;" class="btn btn--m">加入购物车</a>
+                      <a href="javascript:;" class="btn btn--m" @click="addCart(item.productId)">加入购物车</a>
                     </div>
                   </div>
                 </li>
